cdaf4a59bb7d350c34ba3bb4fb0d2d0f935a6240
Justin To <superont@gmail.com>
2024-03-11 13:10:40 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -32,15 +32,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          By{" "}
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={100}
-            height={24}
-            priority
-          />
         </a>
       </footer>
     </main>
