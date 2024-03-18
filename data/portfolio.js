bebe332cde89b52d2e0a3d762b239adb9e6b1018
Justin To <superont@gmail.com>
2024-03-18 14:04:14 -0700

    test

---
+++ data/portfolio.js
@@ -0,0 +1,87 @@
+const about = {
+  // each of these properties is optional - they can be left empty or deleted
+  // each element in the `description` array will be treated as a separate paragraph
+  name: 'John Doe',
+  greetingEmoji: '👋',
+  role: 'Software Engineer',
+  company: 'Acme Labs',
+  description: [
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  ],
+  resume: 'https://example.com',
+  social: {
+    github: 'https://github.com',
+    linkedin: 'https://linkedin.com',
+  },
+}
+
+const projects = [
+  // projects can be added and removed
+  // if the array is left empty, the "Projects" section won't render
+  // each element in each `description` array will be treated as a separate paragraph
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 1',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://example.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 2',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://example.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 3',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://example.com',
+  },
+  {
+    thumbnail: 'images/placeholder-image.jpeg',
+    name: 'Project 4',
+    description: [
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    ],
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://example.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if the array is left empty, the "Skills" section won't render
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Jest',
+  'Enzyme',
+]
+
+const contact = {
+  // the `email` property is optional - it can be left empty or deleted
+  // if so, the "Contact" section won't render
+  email: 'johndoe@email.com',
+}
+
+export { about, projects, skills, contact }
