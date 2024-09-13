09f8f000ce6f038c5489313761dcbdb1ee3175d3
Justin To <superont@gmail.com>
2024-09-12 20:58:03 -0700

    Update portfolio.js

--- data/portfolio.js
+++ data/portfolio.js
@@ -110,8 +110,8 @@ const projects = [
       'A car buying website',
     ],
     stack: ['JavaScript', 'CSS', 'TypeScript', 'React'],
-    sourceCode: '',
-    livePreview: '',
+    sourceCode: 'https://github.com/tocjustin/CarFinder',
+    livePreview: 'carcenter.vercel.app',
   },
   {
     thumbnail: 'images/placeholder-image.jpeg',
